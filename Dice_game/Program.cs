1b6036c8598d6457779e982bd98fb9ade76cc64f
Gunner White <gunnerwhite00@gmail.com>
2022-11-07 16:13:24 -0700

    literally everything

---
+++ Dice_game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.Title = "Practice";
+
+        Console.WriteLine("Hello What's your name?");
+        var Name = Console.ReadLine();
+
+        Console.WriteLine($"Hello {Name}");
+        
+        Console.ReadKey();  
+    }
+  }
+}
